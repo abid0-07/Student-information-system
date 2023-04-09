56758c14b232e4080f5720c58c42c13df5bb3f05
abid0-07 <abid35-757@diu.edu.bd>
2023-04-09 12:01:59 +0600

    User Class added

--- src/Person/Student.java
+++ src/Person/Student.java
@@ -1,4 +1,40 @@
 package Person;
 
 public class Student {
+    private String name;
+    private String Id;
+    private String phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

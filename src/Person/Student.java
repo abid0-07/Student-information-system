ee5ef975e7395035ebf2d16f1cced66faf181913
abid0-07 <abid35-757@diu.edu.bd>
2023-04-09 19:27:44 +0600

    Reading data from files

--- src/Person/Student.java
+++ src/Person/Student.java
@@ -1,5 +1,9 @@
 package Person;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Student {
     private String name;
     private String Id;
@@ -36,5 +40,28 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Student(){
+        Scanner input = new Scanner(System.in);
+        File studentFile = new File("/src/Files/Students.txt");
+        try {
+            Scanner inputFile = new Scanner(studentFile);
+            System.out.print("ID\t");
+            System.out.print("Name\t");
+            System.out.print("phone\t");
+            System.out.println("Email");
+            while(input.hasNext()){
+                String line = inputFile.nextLine();
+                String[] studentInfo = line.split(" ");
+                for (String part:studentInfo){
+                    System.out.print(part+"\t");
+                }
+                System.out.println();
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
